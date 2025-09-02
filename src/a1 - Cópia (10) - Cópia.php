70e8a1a848de126f7c97cb42af26baa011151b27
Nuno Rocha <nuno.rocha@checkmarx.com>
2025-09-02 10:58:17 +0100

    Update a1 - Cópia (10) - Cópia.php

--- src/a1 - Cópia (10) - Cópia.php
+++ src/a1 - Cópia (10) - Cópia.php
@@ -1,4 +1,17 @@
 <?php
-echo"1231231232";
-echo"1231231232";
-?>
+
+if (PHP_SAPI === 'cli') {
+    parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
+
+$file_db = new PDO('sqlite:../database/database.sqlite');
+
+if (NULL == $_GET['id']) $_GET['id'] = 1;
+
+$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
+
+foreach ($file_db->query($sql) as $row) {
+    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
+
+    echo $employee;
+}
